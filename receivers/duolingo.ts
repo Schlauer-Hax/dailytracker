0ef59de78a7fcc1cc8062ea6e733d9cda0aa019c
Schlauer-Hax <hax@bbn.family>
2025-01-23 15:47:18 +0100

    add duo courses

--- receivers/duolingo.ts
+++ receivers/duolingo.ts
@@ -1,5 +1,6 @@
 export async function fetchDuolingo(username: string) {
     const res = await fetch("https://www.duolingo.com/2017-06-30/users?username="+username);
     const json = await res.json();
-    return json.users[0].streakData.currentStreak
+    const user = json.users[0];
+    return {...user.streakData.currentStreak, courses: user.courses}
 }
